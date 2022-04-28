2cd8443f9f151e8af83ef565651197dc6eab8627
fanhuirong <fanhuirong321@gmail.com>
2022-04-28 15:41:13 +0800

    feat: update app.js

--- app.js
+++ app.js
@@ -1,12 +1,18 @@
 const express = require('express')
-require('dotenv').config()
+const process = require('process')
+
 const app = express()
 
 // Heroku dynamically sets a port
-// eslint-disable-next-line no-undef
 const PORT = process.env.PORT || 8080
 
 app.use(express.static('dist'))
 
+app.get('/health', (_, res) => {
+  res.send('ok')
+})
+
 app.listen(PORT, () => {
+  // eslint-disable-next-line no-console
+  console.log('server started on port 5000')
 })
