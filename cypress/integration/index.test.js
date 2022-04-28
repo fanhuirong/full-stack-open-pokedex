eddf53b220386c2c1f3619e30ce7e18d3335c5fc
fanhuirong <fanhuirong321@gmail.com>
2022-04-28 11:05:49 +0800

    feat: step 11.9

---
+++ cypress/integration/index.test.js
@@ -0,0 +1,12 @@
+describe('Pokedex', function() {
+  it('front page can be opened', function() {
+    cy.visit('http://localhost:8080')
+    cy.contains('ivysaur')
+    cy.contains('Pokémon and Pokémon character names are trademarks of Nintendo.')
+  })
+
+  it('go into Pokemon page', () => {
+    cy.get('[href="/pokemon/ivysaur"]').click({ force: true })
+    cy.contains(/chlorophyll/i)
+  })
+})
