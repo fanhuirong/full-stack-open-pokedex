ad3ab77133becdc5c1ec6842b23b3b1dc4f2c2b0
fanhuirong <fanhuirong321@gmail.com>
2022-04-28 11:21:32 +0800

    feat:step 11.9 fix test

--- cypress/integration/index.test.js
+++ cypress/integration/index.test.js
@@ -3,11 +3,11 @@ describe('Pokedex', function() {
   it('front page can be opened', function() {
     cy.visit('http://localhost:5000')
     cy.contains('ivysaur')
-    cy.contains('Pokémon and Pokémon character names are trademarks of Nintendo.')
+    expect(cy.contains('Pokémon and Pokémon character names are trademarks of Nintendo.')).to.be.true
   })
 
   it('go into Pokemon page', () => {
     cy.get('[href="/pokemon/ivysaur"]').click({ force: true })
-    cy.contains(/chlorophyll/i)
+    expect(cy.contains(/chlorophyll/i)).to.be.true
   })
 })
