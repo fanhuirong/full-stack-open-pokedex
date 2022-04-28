ab89ed4243e04ea6b33481ade65ed21ea55bb403
fanhuirong <fanhuirong321@gmail.com>
2022-04-28 12:04:40 +0800

    feat: update package

--- .eslintrc.js
+++ .eslintrc.js
@@ -7,7 +7,7 @@ module.exports = {
   },
   "extends": [
     "eslint:recommended",
-    "plugin:react/recommended,cypress/recommended"
+    "plugin:react/recommended"
     
   ],
   "parserOptions": {
